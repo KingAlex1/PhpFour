da719f3a923b73c9a7c895df8b946233f7fc8d87
Alex B <alex-sert2015@mail.ru>
2018-03-18 22:20:59 +0300

    fixed bags

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 trait Back
 {
     protected function backWayGear()
@@ -7,51 +6,40 @@ trait Back
         echo "Задняя передача" . PHP_EOL;;
     }
 }
-
 trait TransmissionAuto
 {
     use Back;
-
     protected function goforward()
     {
         echo "Режим езды вперед" . PHP_EOL;
         return 1;
     }
 }
-
 trait TransmissionManual
 {
     use Back;
-
     protected function firstGear()
     {
         echo " Едем на первой передаче" . PHP_EOL;
     }
-
     protected function secondGear()
     {
         echo "Едем на вторая передаче" . PHP_EOL;
     }
 }
-
 trait Engine
 {
     private $capacity = 25;
-    protected $temperature;
-
-
+    protected $temperature = 0;
     public function start()
     {
         echo "Запустили двигатель" . PHP_EOL;
-
     }
-
     public function stop()
     {
         echo "Заглушили двигатель" . PHP_EOL;
         echo "Приехали !!!" . PHP_EOL;
     }
-
     public function cooling()
     {
         $this->temperature -= 10;
@@ -59,51 +47,43 @@ trait Engine
         echo "Охлажденная темпиратура " . $this->temperature . PHP_EOL;;
     }
 }
-
-
 class Car
 {
     use Engine ;
     public $distance;
     public $speed;
     public $destination;
-
-
+    public $i = 0;
     public function drive()
     {
         $path = 0;
-        static $i;
         while ($path < $this->distance) {
             sleep(1);
             $path += $this->speed;
             echo "проехали " . $path . " метров " . PHP_EOL;
-            for (; $i < $path; $i = $i + 10) {
+            for (; $this->i < $path; $this->i = $this->i + 10) {
                 $this->temperature += 5;
                 echo "темпиратура двигателя увеличилась на 5 градусов" . PHP_EOL;
-                echo "темпиратура :" . $this->temperature . " градусов " . PHP_EOL;
-
+                echo "темпиратура :" . $this->temperature . " градусов". PHP_EOL;
             }
-            if ($this->temperature > 90) {
+            while ($this->temperature > 90) {
                 $this->cooling();
-
             }
-
         }
     }
 }
-
 class Niva extends Car
-
 {
     use Engine ;
     use TransmissionManual;
-
     function __construct($dist, $speed, $dest)
     {
         $this->distance = $dist;
         $this->speed = $speed;
         $this->destination = $dest;
     }
+
+
 
     function getNiva()
     {
@@ -115,31 +95,24 @@ class Niva extends Car
             $this->firstGear();
             $this->drive();
             $this->stop();
-
         } elseif ($this->destination == 1 && $this->speed > 20) {
             $this->start();
             $this->secondGear();
             $this->drive();
             $this->stop();
-
         } elseif ($this->destination == 0) {
             $this->start();
             $this->backWayGear();
             $this->drive();
             $this->stop();
+
         }
     }
 }
-
-$Nive = new Niva(400, 15, 1);
+$Nive = new Niva(400, 30, 1);
 $Nive->getNiva();
-
-
-
-
-
-
 // dest = 1 = вперед, 0 - назад
+
 
 
 
